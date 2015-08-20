3201cf5d60a17d63d17a360c137a6037d14ba6fc
drmedus <drmedus.lost@gmail.com>
2015-08-20 15:22:39 +0300

    initial

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $(".header-menu").kendoMenu();
+    $(".textButton").kendoButton();
+    $("#admin").kendoButton();
+    $(".kendoSelect").kendoDropDownList();
+});
