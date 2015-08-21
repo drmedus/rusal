297d87c9acbc756423a6b3cc3679e7b1dde17e63
drmedus <drmedus.lost@gmail.com>
2015-08-21 17:04:39 +0300

    Центральная часть главной

--- js/main.js
+++ js/main.js
@@ -4,56 +4,57 @@ $(document).ready(function () {
     $("#admin").kendoButton();
     $(".kendoSelect").kendoDropDownList();
 
-    $("#sidebar").kendoSortable({
-        filter: ">div",
+    $(".sortable-col.wide").kendoSortable({
+        filter: ".drag-widget",
         cursor: "move",
-        connectWith: "#main-content",
+        connectWith: ".narrow-l, .narrow-r",
         placeholder: placeholder,
         hint: hint
     });
 
-    $("#main-content").kendoSortable({
-        filter: ">div",
+    $(".sortable-col.narrow-l").kendoSortable({
+        filter: ".drag-widget",
         cursor: "move",
-        connectWith: "#sidebar",
+        connectWith: ".wide, .narrow-r",
+        placeholder: placeholder,
+        hint: hint
+    });
+
+    $(".sortable-col.narrow-r").kendoSortable({
+        filter: ".drag-widget",
+        cursor: "move",
+        connectWith: ".wide, .narrow-l",
         placeholder: placeholder,
         hint: hint
     });
 
     //exapand
-    $(".panel-wrap").on("click", "span.k-i-arrowhead-s", function(e) {
-        var contentElement = $(e.target).closest(".widget").find(">div");
+    $(".sortable").on("click", "span.k-i-arrowhead-s", function(e) {
+        var contentElement = $(e.target).closest(".drag-widget").find(".widget-content");
         $(e.target)
             .removeClass("k-i-arrowhead-s")
             .addClass("k-i-arrowhead-n");
-
         kendo.fx(contentElement).expand("vertical").stop().play();
     });
 
     //collapse
-    $(".panel-wrap").on("click", "span.k-i-arrowhead-n", function(e) {
-        var contentElement = $(e.target).closest(".widget").find(">div");
+    $(".sortable").on("click", "span.k-i-arrowhead-n", function(e) {
+        var contentElement = $(e.target).closest(".drag-widget").find(".widget-content");
         $(e.target)
             .removeClass("k-i-arrowhead-n")
             .addClass("k-i-arrowhead-s");
-
         kendo.fx(contentElement).expand("vertical").stop().reverse();
     });
 
 
-function placeholder(element) {
+    function placeholder(element) {
     return element.clone().addClass("placeholder");
 }
 
-function hint(element) {
+    function hint(element) {
     return element.clone().addClass("hint")
         .height(element.height())
         .width(element.width());
 }
-
-
-
-
-
 
 });
